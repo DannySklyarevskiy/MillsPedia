68be26d5130842de5504fbc038bf55194726c150
DannySklyarevskiy <137563220+DannySklyarevskiy@users.noreply.github.com>
2023-11-26 22:26:58 -0800

    attempted fix to the images bug

--- mapCreator.js
+++ mapCreator.js
@@ -39,7 +39,7 @@ fetch("markers.tsv")
         //Marker setup
         for (const i of markers) {
             let marker = L.marker([i[3], i[4]]).addTo(map);
-            const nickname = i[5].substr(0, i[5].length - 1);
+            const nickname = i[5].substr(0, i[5].length);
             console.log(nickname);
             marker.bindPopup(
                 "<img class ='image' src=static/images/" +
